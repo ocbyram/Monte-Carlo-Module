36afcc66ba80504ddb02b0362afe51947b06b87b
ocbyram <ocb3wv@virginia.edu>
2024-11-24 11:13:01 -0500

    project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MonteCarlo',
+    version='0.1',
+    description='A Monte Carlo Simulation',
+    url='https://github.com/ocbyram/Monte-Carlo-Module',
+    author='Olivia Byram',
+    author_email='ocbyram@gmail.com',
+    license='MIT',
+    packages=['MonteCarlo']
+)
